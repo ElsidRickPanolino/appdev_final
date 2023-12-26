3f49a919bfb2672497a7c34458ca377ac2242270
elsidRickLopezPanolino <elsidpanolino@gmail.com>
2023-12-26 16:10:27 +0800

    create models and show models in admin

---
+++ projectsite/school/admin.py
@@ -0,0 +1,59 @@
+from django.contrib import admin
+
+from .models import Block, Student, Teacher, Course, Class
+
+# Register your models here.
+
+class StudentInline(admin.TabularInline):
+    model = Student
+    fields = ("student_number", "last_name", "first_name", "middle_name", "email", "contact_number", "birthdate", "address", "image")
+    readonly_fields = ("student_number",)  # Add fields you want to display or make read-only
+
+class PrerequisiteInline(admin.TabularInline):
+    model = Course.prerequisites.through
+    fk_name = 'from_course'
+    verbose_name = 'Prerequisite Course'
+    verbose_name_plural = 'Prerequisite Courses'
+
+@admin.register(Block)
+class BlockAdmin(admin.ModelAdmin):
+    list_display = ("year", "block", "display_name")
+    search_fields = ("display_name",)
+    
+    def display_name(self, obj):
+        return obj.name
+
+    display_name.short_description = 'Name'
+    inlines = [StudentInline]
+    
+    
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_number", "last_name", "first_name", "middle_name", "email", "contact_number", "birthdate", "address","block","image")
+    search_fields = ("student_number",)
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "middle_name", "email", "contact_number", "birthdate", "address","display_courses")
+    search_fields = ("last_name","first_name", "email")
+
+
+    def display_courses(self, obj):
+        return ', '.join([course.name for course in obj.courses.all()])
+
+    display_courses.short_description = 'Courses'
+    
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "description")
+    search_fields = ("code", "name")
+    inlines = [PrerequisiteInline]
+
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ("block", "teacher", "course")
+    search_fields = ("block","course")
---
+++ projectsite/school/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+
+# Create your models here.
+class BaseModel(models.Model): 
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True) 
+    updated_at = models.DateTimeField(auto_now=True) 
+
+    class Meta: 
+        abstract = True
+        
+class Block(BaseModel):
+    year = models.IntegerField()
+    block = models.CharField(max_length=10)
+    
+    @property
+    def name(self):
+        return f"BSCS{self.year} B{self.block}"
+    
+    def __str__(self):
+        return self.name
+    
+class Student(BaseModel):
+    student_number = models.CharField(max_length=9)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    contact_number = models.CharField(max_length=15, null=True, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=250, null=True, blank=True)
+    block = models.ForeignKey(Block, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='student_images/', null=True, blank=True)
+    
+    @property
+    def full_name(self):
+        return f"{self.last_name} {self.first_name}"
+    
+    def __str__(self):
+        return self.full_name
+    
+class Teacher(BaseModel):
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(max_length=100, null=True, blank=True)
+    contact_number = models.CharField(max_length=15, null=True, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=250, null=True, blank=True)
+    courses = models.ManyToManyField('Course')
+    
+    @property
+    def full_name(self):
+        return f"{self.last_name} {self.first_name}"
+    
+    def __str__(self):
+        return self.full_name
+    
+class Course(BaseModel):
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    prerequisites = models.ManyToManyField('self', symmetrical=False, blank=True, verbose_name="Prerequisite Courses")
+    
+    def __str__(self):
+        return self.name
+
+class Class(BaseModel):
+    block = models.ForeignKey(Block, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    
+    @property
+    def name(self):
+        return f"{self.course} - {self.block}"
+    
+    def __str__(self):
+        return self.name
+
+
+
+
